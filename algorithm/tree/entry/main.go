44a50bec7fbd892f698e4a532b13fa1101d04cab
jun <juncwork@gmail.com>
2020-02-01 12:40:55 +0800

    update directory structure

---
+++ algorithm/tree/entry/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"practice/algorithm/tree"
+)
+
+func main() {
+	var root *tree.Node
+	root = &tree.Node{Val: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{Val: 5}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = tree.CreateNode(2)
+	root.Right.Left.SetVal(4)
+
+	fmt.Println(tree.MaxDepth(root))
+}
