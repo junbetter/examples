b8c50ad4b714fa821f22b22a5b3d7e7059b8a179
jun <juncwork@gmail.com>
2020-02-04 13:02:50 +0800

    add context and router

---
+++ framework/core/context.go
@@ -0,0 +1,83 @@
+package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type contextI interface {
+	Status(code int)
+	SetHeader(key string, value string)
+	PostForm(string) string
+	Query(key string) string
+	String(code int, format string, values ...interface{})
+	JSON(code int, obj interface{})
+	Data(code int, data []byte)
+	HTML(code int, html string)
+}
+
+type Context struct {
+	// origin obj
+	w http.ResponseWriter
+	r *http.Request
+	// request info
+	Path   string
+	Method string
+	// response info
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		w:      w,
+		r:      r,
+		Path:   r.URL.Path,
+		Method: r.Method,
+	}
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.w.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.w.Header().Set(key, value)
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.r.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.r.URL.Query().Get(key)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.Status(code)
+	c.SetHeader("Content-Type", "text/plain")
+	c.w.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.Status(code)
+	c.SetHeader("Content-type", "application/json")
+	encoder := json.NewEncoder(c.w)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.w.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.Status(code)
+	c.SetHeader("Content-Type", "text/html")
+	c.w.Write([]byte(html))
+}
--- framework/main.go
+++ framework/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"practice/framework/core"
 )
 
 func main() {
 	r := core.New()
-	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	r.GET("/", func(c *core.Context) {
+		c.HTML(http.StatusOK, "<h1>hello world</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *core.Context) {
+		c.String(http.StatusOK, "hello %s, you are at %s \n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *core.Context) {
+		c.JSON(http.StatusOK, core.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9000")
