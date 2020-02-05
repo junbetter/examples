0e44173f1fdbdc2542d5f3611b6e307548a2947e
jun <juncwork@gmail.com>
2020-02-05 12:40:35 +0800

    add router group

--- framework/core/core.go
+++ framework/core/core.go
@@ -1,37 +1,68 @@
 package core
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(*Context)
 
 type IEngine interface {
-	addRoute(method string, pattern string, handler HandlerFunc)
-	GET(pattern string, handler HandlerFunc)
-	POST(pattern string, handler HandlerFunc)
 	ServeHTTP(w http.ResponseWriter, r *http.Request)
 	Run(addr string) (err error)
 }
 
+type IRouterGroup interface {
+	addRoute(method string, pattern string, handler HandlerFunc)
+	GET(pattern string, handler HandlerFunc)
+	POST(pattern string, handler HandlerFunc)
+	Group(prefix string) *RouterGroup
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share Engine instance
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup // store all groups
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	e.router.addRoute(method, pattern, handler)
+// Group is defined to create a new RouteGroup
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	e := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		engine: e,
+	}
+	e.groups = append(e.groups, newGroup)
+	return newGroup
 }
 
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
+func (g *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
+func (g *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	g.addRoute("GET", pattern, handler)
+}
+
+func (g *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRoute("POST", pattern, handler)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
--- framework/main.go
+++ framework/main.go
@@ -7,21 +7,34 @@ import (
 
 func main() {
 	r := core.New()
-	r.GET("/", func(c *core.Context) {
+	r.GET("/index", func(c *core.Context) {
 		c.HTML(http.StatusOK, "<h1>hello world</h1>")
 	})
 
-	r.POST("/login", func(c *core.Context) {
-		c.JSON(http.StatusOK, core.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *core.Context) {
+			c.HTML(http.StatusOK, "<h1>hello world</h1>")
 		})
-	})
 
-	r.GET("/hello/:name", func(c *core.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+		v1.GET("/hello/:name", func(c *core.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *core.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.POST("/login", func(c *core.Context) {
+			c.JSON(http.StatusOK, core.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	r.GET("/assets/*filepath", func(c *core.Context) {
 		c.JSON(http.StatusOK, core.H{"filepath": c.Param("filepath")})
