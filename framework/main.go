3a79f45b4349119a03942375c6cdcae991e365e8
jun <juncwks@gmail.com>
2020-02-05 14:11:49 +0800

    add middleware

--- framework/main.go
+++ framework/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"examples/framework/core"
+	"log"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() core.HandlerFunc {
+	return func(c *core.Context) {
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.R.RequestURI, time.Since(t))
+
+	}
+}
 
 func main() {
 	r := core.New()
+	r.Use(core.Logger())
 	r.GET("/index", func(c *core.Context) {
 		c.HTML(http.StatusOK, "<h1>hello world</h1>")
 	})
@@ -23,6 +37,7 @@ func main() {
 	}
 
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2())
 	{
 		v2.GET("/hello/:name", func(c *core.Context) {
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
